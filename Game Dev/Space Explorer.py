a1caf31e45f1c814f39a2196c22f5a8fa540dc08
Anato Namikaza <anato.namkikaza.01@gmail.com>
2023-11-10 13:31:56 +0500

    Update Space Explorer.py

--- Game Dev/Space Explorer.py
+++ Game Dev/Space Explorer.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import os
 
 # Initialize Pygame
 pygame.init()
@@ -12,28 +13,33 @@ pygame.display.set_caption("Space Explorer")
 
 # Define colors
 BLACK = (0, 0, 0)
-WHITE = (255, 255, 255)
 
-# Define game elements
-spaceship_image = pygame.image.load("spaceship.png")
+# Function to load and resize images
+def load_and_resize_image(image_path, width, height):
+    image = pygame.image.load(image_path)
+    return pygame.transform.scale(image, (width, height))
+
+# Load and resize game elements
+spaceship_image = load_and_resize_image("spaceship.png", 50, 50)
 spaceship_rect = spaceship_image.get_rect()
 spaceship_rect.centerx = window_width // 2
 spaceship_rect.centery = window_height // 2
 
-obstacle_image = pygame.image.load("obstacle.png")
+obstacle_image = load_and_resize_image("obstacle.png", 50, 50)
 obstacle_rect = obstacle_image.get_rect()
 obstacle_rect.centerx = random.randint(0, window_width)
 obstacle_rect.centery = random.randint(0, window_height)
 
-powerup_image = pygame.image.load("powerup.png")
+powerup_image = load_and_resize_image("powerup.png", 50, 50)
 powerup_rect = powerup_image.get_rect()
 powerup_rect.centerx = random.randint(0, window_width)
 powerup_rect.centery = random.randint(0, window_height)
 
 # Game loop
 running = True
+clock = pygame.time.Clock()
+
 while running:
-    # Handle events
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -46,6 +52,9 @@ while running:
     window.blit(obstacle_image, obstacle_rect)
     window.blit(powerup_image, powerup_rect)
     pygame.display.flip()
+
+    # Cap the frame rate
+    clock.tick(60)
 
 # Quit the game
 pygame.quit()
