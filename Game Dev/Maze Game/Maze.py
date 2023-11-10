85454e6c7a193d0fc880bfd81ba37ff2b29be3bb
Anato Namikaza <anato.namkikaza.01@gmail.com>
2023-11-10 14:08:46 +0500

    Update Maze.py

--- Game Dev/Maze Game/Maze.py
+++ Game Dev/Maze Game/Maze.py
@@ -25,9 +25,27 @@ NUM_COLS = WIDTH // CELL_SIZE
 # Create the maze grid
 maze = [[0] * NUM_COLS for _ in range(NUM_ROWS)]
 
+# Maze generation using recursive backtracking
+def generate_maze(curr_row, curr_col):
+    directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    random.shuffle(directions)
+
+    for dr, dc in directions:
+        new_row, new_col = curr_row + 2 * dr, curr_col + 2 * dc
+        if 0 <= new_row < NUM_ROWS and 0 <= new_col < NUM_COLS and maze[new_row][new_col] == 0:
+            maze[curr_row + dr][curr_col + dc] = 1
+            maze[new_row][new_col] = 1
+            generate_maze(new_row, new_col)
+
 # Set the starting and ending positions
 start_pos = (0, 0)
 end_pos = (NUM_COLS - 1, NUM_ROWS - 1)
+
+# Generate the maze
+generate_maze(0, 0)
+
+# Set up the player position
+player_pos = list(start_pos)
 
 # Set up the game window
 window = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -41,6 +59,22 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    # Move the player with arrow keys
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP] and player_pos[1] > 0 and maze[player_pos[1] - 1][player_pos[0]] == 1:
+        player_pos[1] -= 1
+    if keys[pygame.K_DOWN] and player_pos[1] < NUM_ROWS - 1 and maze[player_pos[1] + 1][player_pos[0]] == 1:
+        player_pos[1] += 1
+    if keys[pygame.K_LEFT] and player_pos[0] > 0 and maze[player_pos[1]][player_pos[0] - 1] == 1:
+        player_pos[0] -= 1
+    if keys[pygame.K_RIGHT] and player_pos[0] < NUM_COLS - 1 and maze[player_pos[1]][player_pos[0] + 1] == 1:
+        player_pos[0] += 1
+
+    # Check if the player reached the end
+    if player_pos == list(end_pos):
+        print("You win!")
+        running = False
+
     # Clear the screen
     window.fill(BLACK)
 
@@ -53,6 +87,9 @@ while running:
     # Draw the start and end positions
     pygame.draw.rect(window, GREEN, (start_pos[0] * CELL_SIZE, start_pos[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
     pygame.draw.rect(window, RED, (end_pos[0] * CELL_SIZE, end_pos[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+
+    # Draw the player
+    pygame.draw.rect(window, BLUE, (player_pos[0] * CELL_SIZE, player_pos[1] * CELL_SIZE, CELL_SIZE, CELL_SIZE))
 
     # Update the display
     pygame.display.update()
